83379df804352175d91ac2ca01bd6a622922df49
Nikita Averochkin <dota.nikita@inbox.ru>
2023-08-07 23:32:58 +0300

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {GoodsComponent} from "./components/goods/goods.component";
+import {ItemsComponent} from "./components/items/items.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "goods", component: GoodsComponent,
+  },
+  {
+    path: "items", component: ItemsComponent
+  }
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/components/items/items.component.ts
@@ -0,0 +1,11 @@
+import {Component} from "@angular/core";
+
+@Component({
+  selector: "items",
+  template: `
+    <h1>Items</h1>
+  `
+})
+export class ItemsComponent {
+
+}
